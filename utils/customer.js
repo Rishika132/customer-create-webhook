25c566d353c1a439e4cc7e295bebf84909f32a38
Rishika132 <rishika01.itgeeks@gmail.com>
2025-08-14 11:46:05 +0530

    14 commit

--- utils/customer.js
+++ utils/customer.js
@@ -24,7 +24,6 @@ async function getCustomerTags(customerId) {
           "X-Shopify-Access-Token": accessToken,
           "Content-Type": "application/json",
         },
-          timeout: 30000,
       }
     );
     return response.data.data.customer?.tags || [];
